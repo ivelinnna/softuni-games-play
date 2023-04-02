0e02a81d26672674da08bb585358b06f58617cd3
Ivelina <ivellinna.kotseva@gmail.com>
2023-04-02 12:34:22 +0300

    Show comments

--- src/App.js
+++ src/App.js
@@ -14,6 +14,20 @@ import * as gameService from './services/gameService';
 function App() {
   const [games, setGames] = useState([]);
 
+  const addComment = (gameId, comment) => {
+    setGames(state => {
+      const game = state.find(x => x._id == gameId);
+
+      const comments = game.comments || [];
+      comments.push(comment)
+
+      return [
+        ...state.filter(x => x._id !== gameId),
+        {...game, comments}
+      ]
+    })
+  }
+
   useEffect(() => {
     gameService.getAll()
       .then(result => {
@@ -33,7 +47,7 @@ function App() {
         <Route path='/register' element={<Register />} />
         <Route path='/create' element={<CreateGame />} />
         <Route path='/catalog' element={<Catalog games={games} />} />
-        <Route path='/catalog/:gameId' element={<GameDetails />} />
+        <Route path='/catalog/:gameId' element={<GameDetails games={games} addComment={addComment}/>} />
 
       </Routes>
     </main>
